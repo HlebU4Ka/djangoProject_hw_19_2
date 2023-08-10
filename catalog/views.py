9bf401392b6077bf597ed70a5a3f6ae3c13c88b8
glebk <glebkot57@gmail.com>
2023-08-10 13:07:02 +0200

    Creat controller page_home & page info

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home_page.html')
+    return render(request, 'catalog/home_page.html')
 
 
 def info_page(request):
-    return render(request, 'info_contact_page.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        massage = request.POST.get('message')
+        print(name, phone, massage)
+    return render(request, 'catalog/info_contact_page.html')
