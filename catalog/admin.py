bfecfd88e2f47b733764358118c1acb75e63652e
glebk <glebkot57@gmail.com>
2023-08-16 18:28:28 +0200

    add class Studentadmin models

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from catalog.models import Student
 
+
 # Register your models here.
-admin.site.register(Student)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_name', 'active')
+    list_filter = ('active',)
+    search_fields = ('name', 'last_name')
--- catalog/models.py
+++ catalog/models.py
@@ -8,7 +8,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100, verbose_name='First Name')
     last_name = models.CharField(max_length=100, verbose_name='Last Name')
     avatar = models.ImageField(upload_to='students/', verbose_name='Avatar', **NULLABLE)
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default=True, verbose_name='учится')
 
     def __str__(self):
         return f'{self.name} {self.last_name}'
